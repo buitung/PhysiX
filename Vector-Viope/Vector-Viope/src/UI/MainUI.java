fd2182fde7b9f5b8f5a2259fbb689e363a4f9b6b
Bui Duy Tung <tungfami91@gmail.com>
2013-05-15 16:57:50 +0300

    Some basic edit

---
+++ Vector-Viope/Vector-Viope/src/UI/MainUI.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author DuyTung
+ */
+public class MainUI extends JFrame{
+    
+}
