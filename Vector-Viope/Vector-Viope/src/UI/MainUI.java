63ca558c9dee47d225c560667d488f1b30e670d9
Bui Duy Tung <tungfami91@gmail.com>
2013-05-15 19:06:12 +0300

    Adding Splash Screen

--- Vector-Viope/Vector-Viope/src/UI/MainUI.java
+++ Vector-Viope/Vector-Viope/src/UI/MainUI.java
@@ -4,12 +4,55 @@
  */
 package UI;
 
-import javax.swing.JFrame;
+import java.awt.SplashScreen;
+import javax.swing.*;
 
 /**
  *
  * @author DuyTung
  */
-public class MainUI extends JFrame{
-    
+public final class MainUI extends JFrame {
+
+    public MainUI() {
+        setSplashScreen();
+        setSize(300, 300);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        addToContentPane();
+    }
+
+    public void setSplashScreen() {
+        final SplashScreen splash = SplashScreen.getSplashScreen();
+        if (splash != null) {
+            try {
+                Thread.sleep(1500);             // wait a second
+            } catch (InterruptedException ex) {
+            }
+            splash.close();
+        }
+    }
+
+    public void addToContentPane() {
+    }
+
+    public static void main(String[] args) {
+        //Set the Nimbus look and feel
+        try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException | ClassNotFoundException ex) {
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainUI mainUI = new MainUI();
+            }
+        });
+    }
 }
