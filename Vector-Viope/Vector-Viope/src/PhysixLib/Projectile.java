70ff9afd403c28803fe90cd5760a54cf0ac61eb0
Tung Trinh <Tung.Trinh@metropolia.fi>
2013-05-21 16:18:58 +0300

    asdf

--- Vector-Viope/Vector-Viope/src/PhysixLib/Projectile.java
+++ Vector-Viope/Vector-Viope/src/PhysixLib/Projectile.java
@@ -5,11 +5,8 @@ package PhysixLib;
  */
 public class Projectile {
 
-    
     /**
-     * Velocity 
-     * Angle
-     * Gravitational acceleration
+     * Velocity Angle Gravitational acceleration
      */
     private float velocity;
     private float angle;
@@ -23,20 +20,19 @@ public class Projectile {
     }
 
     /**
-     * 
+     *
      * @param v velocity
-     * @param a angle
-     * Constructor
+     * @param a angle Constructor
      */
     public Projectile(float v, float a) {
         this.velocity = v;
         this.angle = a;
     }
 
-
     /**
      *
-     * @return the float value of the duration a projectile would spend in the air providing a float angle and a float velocity. 
+     * @return the float value of the duration a projectile would spend in the
+     * air providing a float angle and a float velocity.
      */
     public float ProjTime() {
         return 2 * velocity * ((float) Math.sin(angle)) / g;
@@ -44,11 +40,13 @@ public class Projectile {
 
     /**
      *
-     * @return the float value of the distance a projectile would travel providing a float angle and a float velocity.
+     * @return the float value of the distance a projectile would travel
+     * providing a float angle and a float velocity.
      */
     public float Distance() {
         float ss;
-        if (angle == 0 || angle == MathUtils.Deg2Rad(180)) {
+        if (angle == 0 || angle == MathUtils.Deg2Rad(180)
+                || angle == MathUtils.Deg2Rad(90)) {
             ss = 0;
         } else {
             ss = (float) Math.sin(2 * angle);
@@ -58,7 +56,8 @@ public class Projectile {
 
     /**
      *
-     * @return the float value of the height a projectile would reach providing a float angle and a float velocity.
+     * @return the float value of the height a projectile would reach providing
+     * a float angle and a float velocity.
      */
     public float Height() {
         return (float) Math.pow(velocity, 2)
